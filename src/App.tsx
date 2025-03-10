acc47085c057a1b7cc89e069e68e000c7317fce4
ivantelizhenko <telizenkoo@gmail.com>
2025-03-10 10:00:43 +0200

    small style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+
+import Footer from './components/Footer';
+import Main from './components/Main';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: pink;
+  height: 100%;
+`;
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Wrapper>
+      <Main />
+      <Footer />
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ src/components/BookForm.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledBookForm = styled.form`
+  /* height: 100%; */
+  background-color: lightblue;
+`;
+
+function BookForm() {
+  return <StyledBookForm>Form</StyledBookForm>;
+}
+
+export default BookForm;
